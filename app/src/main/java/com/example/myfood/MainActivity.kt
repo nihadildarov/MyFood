9531694c12d41e96228ad49b8651014c3d22cce8
Nihad Ildarov <123246218+nihadildarov@users.noreply.github.com>
2023-12-09 16:53:55 +0400

    Initial project

---
+++ app/src/main/java/com/example/myfood/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.myfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btmNavController ()
+    }
+
+
+    private fun btmNavController (){
+        val navHost = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHost.navController
+        binding.btmNav.setupWithNavController(navController)
+    }
+}
